ced6a946208e1f2056e6abe9cbf2ebbee0eb94d2
Karin Fernandez <karinfdez@yahoo.com>
2016-04-13 12:38:04 -0400

    First commit with only a few directives

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('photoAlbum', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr']);
+
+})();
