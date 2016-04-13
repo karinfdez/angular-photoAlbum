0105cd694103d1938c2518f22284ad240506c70a
Karin Fernandez <karinfdez@yahoo.com>
2016-04-13 14:16:41 -0400

    Added message when album is not found. Passes album to the controller

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,31 +9,36 @@
   function MainController($timeout, webDevTec, toastr) {
     var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1460392585454;
-    vm.showToastr = showToastr;
 
-    activate();
+    vm.albums=[{name: 'Madrid2009',title:'Weekend in Madrid',date:'2009-09-01',description:'Fun weekend visiting the city'},
+               {name: 'Brazil2010',title:'A week in Rio de Janeiro',date:'2010-05-23',description:'Week with friends and family'},
+               {name: 'Cuba2011',title:'Visiting Habana',date:'2011-10-10',description:'Havana with my parents'},
+               {name: 'Egypt2012',title:'Visitng El Cairo',date:'2011-11-01',description:'Amazing piramides'}];
+    // vm.awesomeThings = [];
+    // vm.classAnimation = '';
+    // vm.creationDate = 1460392585454;
+    // vm.showToastr = showToastr;
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
+    // activate();
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
+    // function activate() {
+    //   getWebDevTec();
+    //   $timeout(function() {
+    //     vm.classAnimation = 'rubberBand';
+    //   }, 4000);
+    // }
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
+    // function showToastr() {
+    //   toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    //   vm.classAnimation = '';
+    // }
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
+    // function getWebDevTec() {
+    //   vm.awesomeThings = webDevTec.getTec();
+
+    //   angular.forEach(vm.awesomeThings, function(awesomeThing) {
+    //     awesomeThing.rank = Math.random();
+    //   });
+    // }
   }
 })();
