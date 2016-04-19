764f7598dc4ee4c7e852e538648b10909be9ba3b
Karin Fernandez <karinfdez@yahoo.com>
2016-04-19 11:15:00 -0400

    Added for to add album

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,7 +6,7 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($timeout, webDevTec) {
     var vm = this;
 
 
@@ -14,6 +14,11 @@
                {name: 'Brazil2010',title:'A week in Rio de Janeiro',date:'2010-05-23',description:'Week with friends and family'},
                {name: 'Cuba2011',title:'Visiting Habana',date:'2011-10-10',description:'Havana with my parents'},
                {name: 'Egypt2012',title:'Visitng El Cairo',date:'2011-11-01',description:'Amazing piramides'}];
+    
+    vm.addAlbum=function(new_album){
+      vm.albums.push(new_album);
+      vm.adding_album={};
+    }
     // vm.awesomeThings = [];
     // vm.classAnimation = '';
     // vm.creationDate = 1460392585454;
